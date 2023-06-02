99b1c2d836da31784fbdce432b7006b4d8cf017a
feelgoodseven <112790813+feelgoodseven@users.noreply.github.com>
2023-06-02 22:57:55 +0900

    파일이름 년월일 이름으로 저장하고 키보드로 돈,내돈 국가코드,바꿀돈 국가코드 입력받도록 수정

--- exchange_app/main.c
+++ exchange_app/main.c
@@ -8,52 +8,12 @@
 
 char* baseUrl = "https://nginx-nginx-r8xoo2mleehqmty.sel3.cloudtype.app/rate/";
 
-char* fileName_String() {
-    char file_name[20] = "";
-    struct tm* t;
-    time_t base = time(NULL);
-    t = localtime(&base);
-    
-    int year = t->tm_year + 1900;
-    int month = t->tm_mon + 1;
-    int day = t->tm_mday;
-    
-    char year_string[10];
-    char month_string[10];
-    char day_string[10];
-
-    //sprintf(year_string, "%s", year);
-    //sprintf(month_string, "%s", month);
-    //sprintf(day_string, "%s", day);
-    
-    _itoa(year, year_string, 10);
-    _itoa(month, month_string, 10);
-    _itoa(day, day_string, 10);
-   
-
-    strcat(file_name, year_string);
-    strcat(file_name, month_string);
-    strcat(file_name, day_string);
-    strcat(file_name, ".json");
-
-
-
-    printf("%s\n", file_name);
-
-    return file_name;
-
-    
-}
-
-void save_jobjFile(char * jsonData) {
-    char *file_name= NULL;
-    file_name= fileName_String();
-    printf("%s\n", file_name);
-
-    //FILE* fp = fopen(fileName_String(), "w"); //test파일을 w(쓰기) 모드로 열기
-    //fputs(jsonData,fp);
-    //fclose(fp);
-}
+struct CurrencyCode
+{
+    char JPY[4];
+    char USD[4];
+    char KRW[4];
+};
 
 size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
     size_t realsize = size * nmemb;
@@ -64,8 +24,144 @@ size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
     return realsize;
 }
 
+struct CurrencyCode initStruct() {
+    struct CurrencyCode code;
+    strcpy(code.JPY,"JPY");
+    strcpy(code.KRW, "KRW");
+    strcpy(code.USD, "USD");
+
+    return code;
+
+}
+
+char* selectCode() {
+    //가지고 있는 돈의 번호를 선택
+    //ex 1.한국 2.미국 3.일본 등등
+    int selectCodeNum;
+    static char selectCodeString[4]="";
+
+    struct CurrencyCode currencyCode;
+
+    //구조체를 초기화 하는 함수를 실행해서
+    //currencyCode 구조체에 값을 넣는다.
+    currencyCode = initStruct();
+
+    
+    printf("가지고 있는 돈의 국가를 선택해주세요.\n");
+    printf("1.한국 2.미국 3.일본:");
+    scanf("%d", &selectCodeNum);
+
+    if (selectCodeNum == 1) {
+        strcpy(selectCodeString, currencyCode.KRW);
+        return selectCodeString;
+    }
+    else if (selectCodeNum == 2) {
+        strcpy(selectCodeString, currencyCode.USD);
+        return selectCodeString;
+    }
+    else if (selectCodeNum == 3) {
+        strcpy(selectCodeString, currencyCode.JPY);
+        return selectCodeString;
+    }
+}
+
+char* targetSelectCode() {
+    //가지고 있는 돈의 번호를 선택
+    //ex 1.한국 2.미국 3.일본 등등
+    int targetSelectCodeNum;
+    static char targetSelectCodeString[4] = "";
+
+    struct CurrencyCode currencyCode;
+
+    //구조체를 초기화 하는 함수를 실행해서
+    //currencyCode 구조체에 값을 넣는다.
+    currencyCode = initStruct();
+
+
+    printf("바꿀 돈의 국가를 선택해주세요.\n");
+    printf("1.한국 2.미국 3.일본:");
+    scanf("%d", &targetSelectCodeNum);
+
+    if (targetSelectCodeNum == 1) {
+        strcpy(targetSelectCodeString, currencyCode.KRW);
+        return targetSelectCodeString;
+    }
+    else if (targetSelectCodeNum == 2) {
+        strcpy(targetSelectCodeString, currencyCode.USD);
+        return targetSelectCodeString;
+    }
+    else if (targetSelectCodeNum == 3) {
+        strcpy(targetSelectCodeString, currencyCode.JPY);
+        return targetSelectCodeString;
+    }
+}
+
+
+//파일이름을 현재 년월일로 저장하기위해
+//파일 이름을 만들어 주는 함수
+char* fileName_String() {
+    //함수가 끝나도 변수가 남아서 return 해줘야하기 때문에
+    //static 정적 변수로 만들어 준다.
+    char *file_name_pointer= malloc(sizeof(char) * 20);
+    char file_name[20] = "";
+
+    struct tm* t;
+    time_t base = time(NULL);
+    t = localtime(&base);
+
+    int year = t->tm_year + 1900;
+    int month = t->tm_mon + 1;
+    int day = t->tm_mday;
+
+    //년도 글자를 넣기위한 변수
+    char year_string[10];
+    //월 글자를 넣기 위한 변수
+    char month_string[10];
+    //일 글자를 넣기 위한 변수
+    char day_string[10];
+
+    //년도 숫자형을 문자형으로 변경하기위한 함수
+    //숫자형,문자형을 넣기위한 변수,진수(8진수,16진수,10진수)
+    _itoa(year, year_string, 10);
+    _itoa(month, month_string, 10);
+    _itoa(day, day_string, 10);
+
+
+    //문자 연결하기 위한 함수
+    strcat(file_name, year_string);
+    strcat(file_name, month_string);
+    strcat(file_name, day_string);
+    strcat(file_name, ".json");
+
+    //동적메모리는 포인터를 활용하므로
+    //동적메모리로 지정된 변수에 글자를 집어넣으려면
+    //넣을 값을 복사해서 동적메모리 변수에 넣어야한다.
+    strcpy(file_name_pointer, file_name);
+
+    return file_name_pointer;
+}
+
+//환율 비율 저장하기 위한 함수
+void save_jobjFile(char * jsonData) {
+    char* file_name_pointer;
+    file_name_pointer = fileName_String();
+    printf("파일이름:%s\n", file_name_pointer);
+
+
+    FILE* fp = fopen(file_name_pointer, "w");
+    fputs(jsonData,fp);
+    fclose(fp);
+    
+    //동적 메모리로 지정해 놓아서 값이 남아 있다.
+    //파일 이름이 남아있으면 파일이름이 두번 찍히므로
+    //파일이름 변수를 초기화 해줘야한다.
+    free(file_name_pointer);
+}
+
+
 void exchange_api_request(char *currencyCode,char* selectCode,int money){
-    printf("%s\n", currencyCode);
+    //printf("%s\n", currencyCode);
+
     CURL* curl;
     CURLcode res;
 
@@ -75,9 +171,6 @@ void exchange_api_request(char *currencyCode,char* selectCode,int money){
     strcpy(url, baseUrl);
 
     strcat(url, currencyCode);
-
-    
-
 
     printf("baseurl:%s\n", url);
     
@@ -141,13 +234,6 @@ void exchange_api_request(char *currencyCode,char* selectCode,int money){
             struct json_object* rate_obj;
 
 
-
-            
-
-
-
-
-
             json_object_object_get_ex(jobj, "rate_data", &exchang_rate_obj);
 
             json_object_object_get_ex(exchang_rate_obj, "rates",&all_rate_obj);
@@ -166,22 +252,48 @@ void exchange_api_request(char *currencyCode,char* selectCode,int money){
             //환율 계산 결과
             double exchang_result = rate_value * money;
 
-            printf("계산결과: %f\n", exchang_result);
-            
+            printf("계산결과: %f ", exchang_result);
+            printf("%s\n", selectCode);
 
         }
     }
 
     free(url);
     free(response_buf);
-
-
 }
 
 
 int main() {
-    //KRW 베이스 돈,USD 환전하려는 나라의 돈,5000 얼마나 환전할건지
-    exchange_api_request("KRW","USD",5000);
+
+    char* select_Code_Value;
+    char* target_Code_Value;
+    int money=0;
+
+    //국가코드 구조체로 설정
+    struct CurrencyCode currencyCode;
+
+    //구조체를 초기화 하는 함수를 실행해서
+    //currencyCode 구조체에 값을 넣는다.
+    currencyCode = initStruct();
+
+
+    while (1) {
+        //나의 돈 나라 코드
+        select_Code_Value = selectCode();
+
+        //바꾸려는 돈의 나라 코드
+        target_Code_Value = targetSelectCode();
+
+        printf("돈입력(%s):",select_Code_Value);
+        scanf("%d", &money);
+
+        printf("나의돈 코드:%s\n", select_Code_Value);
+        printf("바꿀돈 코드:%s\n", target_Code_Value);
+
+        //KRW 베이스 돈,USD 환전하려는 나라의 돈,5000 얼마나 환전할건지
+        exchange_api_request(select_Code_Value, target_Code_Value, money);
+    }
+
     system("pause > nul");
 	return 0;
 }
